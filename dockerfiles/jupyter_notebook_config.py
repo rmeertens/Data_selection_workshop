719448193785a91cd1ccd9f4ac22204c8c82eea6
Roland Meertens <rolandmeertens@gmail.com>
2021-05-08 14:59:10 +0200

    🥳First examples for data selection ready

---
+++ dockerfiles/jupyter_notebook_config.py
@@ -0,0 +1,12 @@
+import os
+from IPython.lib import passwd
+
+c.NotebookApp.ip = '*'
+c.NotebookApp.port = int(os.getenv('PORT', 8888))
+c.NotebookApp.open_browser = False
+c.MultiKernelManager.default_kernel_name = 'python2'
+c.NotebookApp.password = '' 
+c.NotebookApp.token = ''
+
+c.NotebookApp.allow_remote_access = True
+c.NotebookApp.allow_origin = '*'
